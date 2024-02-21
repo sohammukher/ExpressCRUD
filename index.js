0838fa113afc547fb162c81af7bfeaa562b83653
sohammukher <sohammukherjee1996@gmail.com>
2024-02-21 01:49:12 -0500

    Models

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import express from 'express';
 import routes from "./src/routes/crmRoutes"
 
+// imports for the DB
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser'
+
+
+
 // Run Express
  const app = express();
+
+// mongoose connection
+
+// Promise to wait for response as opposed to turn an error until they get the answer.
+mongoose.Promise = global.Promise;  // to support using native promises (i.e., global.Promise this is ), which provides better compatibility with modern JavaScript environments.
+
+mongoose.connect('mongodb://localhost/NewConnection');
+
+
+
+
 
 // Run our routes:
 routes(app);
 
 
  // Define Port
-const PORT = 3001;
+const PORT = 9999;
 
  app.get('/', (req,res)=>{
  
---
+++ src/models/crmModel.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose"
+
+
+const Schema = mongoose.Schema;
+
+export const ContactSchema = new Schema({
+    firstName:{
+        type:String,
+        required:"Enter first name"
+    },
+    lastName:{
+        type:String,
+        required:"Enter last name"
+    },
+    email:{
+        type:String
+    },
+    phone:{
+        type:Number
+    }
+})
